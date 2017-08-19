02c26c8127a09452bcc9360cbb618ce0740d04c0
Nam Luu <nam.luuduc@gmail.com>
2017-08-19 21:19:23 +0700

    remove bongda

--- bongda/get.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file = file_get_contents('http://sportstream365.com/LiveFeed/GetLeftMenuShort?sports=1&lng=vi&partner=24');
-
-echo "Thời gian cập nhật ".gmdate('Y-m-d H:i:s')."<br>";
-echo 'Trận đấu đã được cập nhật, data:'.file_put_contents(dirname(__FILE__).'/LiveFeed.json', $file);
-?>
-<p><a href="<?php echo $_SERVER['HTTP_REFERER'] ?>">Quay lại trang danh sách trận đấu</a></p>
--- bongda/getgame.php
+++
@@ -1,5 +0,0 @@
-<?php
-//http://sportstream365.com/LiveFeed/GetGame?lng=vi&id=72786975&partner=24&_=1458751270218
-$file = file_get_contents('http://sportstream365.com/LiveFeed/GetGame?lng=vi&partner=24&id='.$_GET['id']);
-echo $file;
-?>
